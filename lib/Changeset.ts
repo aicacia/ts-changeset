e7e5f454f05e17a7f2bc6d32e3cf3cb0c9dba98d
Nathan Faucett <nathanfaucett@gmail.com>
2019-01-12 09:05:14 -0500

    clean up

--- lib/Changeset.ts
+++ lib/Changeset.ts
@@ -1,211 +1,211 @@
 export interface IChanges {
-    [key: string]: any;
+  [key: string]: any;
 }
 
 export interface IError {
-    message: string;
-    keys: any[];
+  message: string;
+  keys: any[];
 }
 
 export interface IErrors {
-    [key: string]: IError[];
+  [key: string]: IError[];
 }
 
 export interface IModified {
-    [key: string]: boolean;
+  [key: string]: boolean;
 }
 
 export class Changeset {
-    defaults: IChanges;
-    changes: IChanges;
-    errors: IErrors;
-    modified: IModified;
-    valid: boolean;
+  private _defaults: IChanges;
+  private _changes: IChanges;
+  private _errors: IErrors;
+  private _modified: IModified;
+  private _valid: boolean;
 
-    constructor(defaults: IChanges) {
-        this.defaults = defaults;
-        this.changes = this.defaults;
-        this.errors = {};
-        this.modified = {};
-        this.valid = true;
+  constructor(defaults: IChanges) {
+    this._defaults = defaults;
+    this._changes = this._defaults;
+    this._errors = {};
+    this._modified = {};
+    this._valid = true;
+  }
+
+  isValid() {
+    return this._valid;
+  }
+
+  isInvalid() {
+    return !this.isValid();
+  }
+
+  hasChange(field: string) {
+    return !!this._changes[field];
+  }
+
+  getChange<T = any>(field: string, defaultValue?: T): T | undefined {
+    if (this._changes.hasOwnProperty(field)) {
+      return this._changes[field];
+    } else {
+      return defaultValue;
+    }
+  }
+
+  getModified(field: string): boolean {
+    return !!this._modified[field];
+  }
+
+  getError(field: string): IError[] {
+    return this._errors[field] || [];
+  }
+
+  getErrors(): IErrors {
+    return this._errors;
+  }
+
+  getChanges(): IChanges {
+    return this._changes;
+  }
+
+  addError(field: string, message: string, keys: any[] = []): Changeset {
+    const errors = this._errors[field] || (this._errors[field] = []);
+
+    errors.push({
+      message,
+      keys
+    });
+
+    this._valid = false;
+
+    return this;
+  }
+
+  addChange<T = any>(field: string, value: T): Changeset {
+    this._changes[field] = value;
+    this._modified[field] = true;
+    return this;
+  }
+
+  addChanges(changes: IChanges): Changeset {
+    Object.keys(changes).forEach(key => {
+      this.addChange(key, changes[key]);
+    });
+    return this;
+  }
+
+  addDefault<T = any>(field: string, value: T): Changeset {
+    this._defaults[field] = value;
+    return this;
+  }
+
+  addDefaults(defaults: IChanges): Changeset {
+    Object.keys(defaults).forEach(key => {
+      this.addDefault(key, defaults[key]);
+    });
+    return this;
+  }
+
+  clearErrors(): Changeset {
+    this._errors = {};
+    this._valid = true;
+    return this;
+  }
+
+  clearChanges(): Changeset {
+    this._changes = this._defaults;
+    this._modified = {};
+    return this;
+  }
+
+  clearDefaults(): Changeset {
+    this._defaults = {};
+    return this;
+  }
+
+  clear(): Changeset {
+    this.clearErrors();
+    this.clearChanges();
+    return this;
+  }
+
+  filter(fields: string[]): Changeset {
+    const changes: IChanges = {},
+      errors: IErrors = {},
+      modified: IModified = {};
+
+    fields.forEach(field => {
+      changes[field] = this.getChange(field);
+      errors[field] = this.getError(field);
+      modified[field] = true;
+    });
+
+    this._changes = changes;
+    this._errors = errors;
+    this._modified = modified;
+
+    return this;
+  }
+
+  validateAcceptance(field: string): Changeset {
+    const value: boolean = !!this.getChange(field);
+
+    if (value !== true) {
+      this.addError(field, "acceptance");
     }
 
-    isValid() {
-        return this.valid;
-    }
+    return this;
+  }
 
-    isInvalid() {
-        return !this.isValid();
-    }
+  validateLength(
+    field: string,
+    opts: { [key: string]: number } = {}
+  ): Changeset {
+    let value = this.getChange(field);
 
-    hasChange(field: string) {
-        return !!this.changes[field];
-    }
+    value = value == null ? [] : value;
 
-    getChange<T = any>(field: string, defaultValue?: T): T | undefined {
-        if (this.changes.hasOwnProperty(field)) {
-            return this.changes[field];
-        } else {
-            return defaultValue;
+    const length =
+      value && typeof value.length === "number" ? +value.length : value;
+
+    Object.keys(opts).forEach((op: string) => {
+      const comparision = COMPARISIONS[op];
+
+      if (comparision) {
+        const opLength = +opts[op];
+
+        if (!comparision(length, opLength)) {
+          this.addError(field, "length", [op, opLength]);
         }
+      } else {
+        throw new TypeError("No comparision for " + op);
+      }
+    });
+
+    return this;
+  }
+
+  validateRequired(values: string[]): Changeset {
+    values.forEach((field: string) => {
+      const value = this.getChange(field);
+
+      if (value == null || value === "") {
+        this.addError(field, "required");
+      }
+    });
+
+    return this;
+  }
+
+  validateFormat(field: string, regex: RegExp): Changeset {
+    let value = this.getChange(field);
+
+    value = value == null ? "" : value.toString();
+
+    if (!regex.test(value)) {
+      this.addError(field, "format", [regex]);
     }
 
-    getModified(field: string): boolean {
-        return !!this.modified[field];
-    }
-
-    getError(field: string): IError[] {
-        return this.errors[field] || [];
-    }
-
-    getErrors(): IErrors {
-        return this.errors;
-    }
-
-    getChanges(): IChanges {
-        return this.changes;
-    }
-
-    addError(field: string, message: string, keys: any[] = []): Changeset {
-        const errors = this.errors[field] || (this.errors[field] = []);
-
-        errors.push({
-            message,
-            keys
-        });
-
-        this.valid = false;
-
-        return this;
-    }
-
-    addChange<T = any>(field: string, value: T): Changeset {
-        this.changes[field] = value;
-        this.modified[field] = true;
-        return this;
-    }
-
-    addChanges(changes: IChanges): Changeset {
-        Object.keys(changes).forEach(key => {
-            this.addChange(key, changes[key]);
-        });
-        return this;
-    }
-
-    addDefault<T = any>(field: string, value: T): Changeset {
-        this.defaults[field] = value;
-        return this;
-    }
-
-    addDefaults(defaults: IChanges): Changeset {
-        Object.keys(defaults).forEach(key => {
-            this.addDefault(key, defaults[key]);
-        });
-        return this;
-    }
-
-    clearErrors(): Changeset {
-        this.errors = {};
-        this.valid = true;
-        return this;
-    }
-
-    clearChanges(): Changeset {
-        this.changes = this.defaults;
-        this.modified = {};
-        return this;
-    }
-
-    clearDefaults(): Changeset {
-        this.defaults = {};
-        return this;
-    }
-
-    clear(): Changeset {
-        this.clearErrors();
-        this.clearChanges();
-        return this;
-    }
-
-    filter(fields: string[]): Changeset {
-        const changes: IChanges = {},
-            errors: IErrors = {},
-            modified: IModified = {};
-
-        fields.forEach(field => {
-            changes[field] = this.getChange(field);
-            errors[field] = this.getError(field);
-            modified[field] = true;
-        });
-
-        this.changes = changes;
-        this.errors = errors;
-        this.modified = modified;
-
-        return this;
-    }
-
-    validateAcceptance(field: string): Changeset {
-        const value: boolean = !!this.getChange(field);
-
-        if (value !== true) {
-            this.addError(field, "acceptance");
-        }
-
-        return this;
-    }
-
-    validateLength(
-        field: string,
-        opts: { [key: string]: number } = {}
-    ): Changeset {
-        let value = this.getChange(field);
-
-        value = value == null ? [] : value;
-
-        const length =
-            value && typeof value.length === "number" ? +value.length : value;
-
-        Object.keys(opts).forEach((op: string) => {
-            const comparision = COMPARISIONS[op];
-
-            if (comparision) {
-                const opLength = +opts[op];
-
-                if (!comparision(length, opLength)) {
-                    this.addError(field, "length", [op, opLength]);
-                }
-            } else {
-                throw new TypeError("No comparision for " + op);
-            }
-        });
-
-        return this;
-    }
-
-    validateRequired(values: string[]): Changeset {
-        values.forEach((field: string) => {
-            const value = this.getChange(field);
-
-            if (value == null || value === "") {
-                this.addError(field, "required");
-            }
-        });
-
-        return this;
-    }
-
-    validateFormat(field: string, regex: RegExp): Changeset {
-        let value = this.getChange(field);
-
-        value = value == null ? "" : value.toString();
-
-        if (!regex.test(value)) {
-            this.addError(field, "format", [regex]);
-        }
-
-        return this;
-    }
+    return this;
+  }
 }
 
 const eq = (a: number, b: number) => a === b;
@@ -218,16 +218,16 @@ const gt = (a: number, b: number) => a > b;
 const lt = (a: number, b: number) => a < b;
 
 const COMPARISIONS: { [key: string]: (a: number, b: number) => boolean } = {
-    "==": eq,
-    ">=": gte,
-    "<=": lte,
-    "!=": neq,
-    ">": gt,
-    "<": lt,
-    eq: eq,
-    gte: gte,
-    lte: lte,
-    neq: neq,
-    gt: gt,
-    lt: lt
+  "==": eq,
+  ">=": gte,
+  "<=": lte,
+  "!=": neq,
+  ">": gt,
+  "<": lt,
+  eq,
+  gte,
+  lte,
+  neq,
+  gt,
+  lt
 };
--- tests/Changeset.test.ts
+++ tests/Changeset.test.ts
@@ -2,62 +2,62 @@ import * as tape from "tape";
 import { Changeset } from "../lib";
 
 const changesetFn = (changeset: Changeset) =>
-    changeset
-        .filter(["age", "name", "agreedToTerms"])
-        .validateAcceptance("agreedToTerms")
-        .validateLength("age", { gt: 18 })
-        .validateFormat("name", /[A-Za-z0-9\-_]+/)
-        .validateRequired(["age", "name", "agreedToTerms"]);
+  changeset
+    .filter(["age", "name", "agreedToTerms"])
+    .validateAcceptance("agreedToTerms")
+    .validateLength("age", { gt: 18 })
+    .validateFormat("name", /[A-Za-z0-9\-_]+/)
+    .validateRequired(["age", "name", "agreedToTerms"]);
 
 tape("Changeset valid", (assert: tape.Test) => {
-    const changeset = new Changeset({
-        age: 0,
-        name: "",
-        agreedToTerms: false
-    });
+  const changeset = new Changeset({
+    age: 0,
+    name: "",
+    agreedToTerms: false
+  });
 
-    changeset.addChanges({
-        age: 20,
-        name: "Nathan",
-        agreedToTerms: true
-    });
-    changesetFn(changeset);
+  changeset.addChanges({
+    age: 20,
+    name: "Nathan",
+    agreedToTerms: true
+  });
+  changesetFn(changeset);
 
-    assert.equals(changeset.isValid(), true);
-    assert.deepEquals(changeset.getChanges(), {
-        age: 20,
-        name: "Nathan",
-        agreedToTerms: true
-    });
+  assert.equals(changeset.isValid(), true);
+  assert.deepEquals(changeset.getChanges(), {
+    age: 20,
+    name: "Nathan",
+    agreedToTerms: true
+  });
 
-    assert.end();
+  assert.end();
 });
 
 tape("Changeset invalid", (assert: tape.Test) => {
-    const changeset = new Changeset({
-        age: 0,
-        name: "",
-        agreedToTerms: false
-    });
+  const changeset = new Changeset({
+    age: 0,
+    name: "",
+    agreedToTerms: false
+  });
 
-    changeset.addChanges({
-        age: 15,
-        name: "%#%$%@",
-        agreedToTerms: false
-    });
-    changesetFn(changeset);
+  changeset.addChanges({
+    age: 15,
+    name: "%#%$%@",
+    agreedToTerms: false
+  });
+  changesetFn(changeset);
 
-    assert.equals(changeset.isValid(), false);
-    assert.deepEquals(changeset.getChanges(), {
-        age: 15,
-        name: "%#%$%@",
-        agreedToTerms: false
-    });
-    assert.deepEquals(changeset.getErrors(), {
-        age: [{ message: "length", keys: ["gt", 18] }],
-        name: [{ message: "format", keys: [/[A-Za-z0-9\-_]+/] }],
-        agreedToTerms: [{ message: "acceptance", keys: [] }]
-    });
+  assert.equals(changeset.isValid(), false);
+  assert.deepEquals(changeset.getChanges(), {
+    age: 15,
+    name: "%#%$%@",
+    agreedToTerms: false
+  });
+  assert.deepEquals(changeset.getErrors(), {
+    age: [{ message: "length", keys: ["gt", 18] }],
+    name: [{ message: "format", keys: [/[A-Za-z0-9\-_]+/] }],
+    agreedToTerms: [{ message: "acceptance", keys: [] }]
+  });
 
-    assert.end();
+  assert.end();
 });
